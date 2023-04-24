07fd8a9d8300dd9cec46a24af07e268e5c45212a
mimarz <mimarz@users.noreply.github.com>
2023-04-24 12:45:30 +0000

    Deploying to github-pages-deployment from @ digdir/designsystem@a4b880edf75ca5e06e30388af42fd78807bed192 🚀

---
+++ docs/sb-addons/storybook-10/manager-bundle.mjs
@@ -0,0 +1,4 @@
+try{
+var _=__STORYBOOKADDONS__,{addons:t,types:p,mockChannel:f}=__STORYBOOKADDONS__;var b=__STORYBOOKTHEMING__,{CacheProvider:x,ClassNames:S,Global:u,ThemeProvider:B,background:C,color:y,convert:R,create:a,createCache:N,createGlobal:k,createReset:D,css:G,darken:K,ensure:Y,ignoreSsrWarning:v,isPropValid:A,jsx:I,keyframes:P,lighten:w,styled:E,themes:H,typography:M,useTheme:j,withTheme:V}=__STORYBOOKTHEMING__;var s=a({brandTitle:"@digdir",fontBase:'"Inter", sans-serif',base:"light",colorSecondary:"#0062BA",barTextColor:"#68707c",appBg:"#f4f5f6",textColor:"#1e2b3c"});t.setConfig({theme:s,sidebar:{showRoots:!1}});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.mjs.map
